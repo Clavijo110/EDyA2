1fa5312c10bfcc24c56909191bd2a259febe586e
Clavijo_110 <alejandro.clavijo_a@uao.edu.co>
2024-09-05 12:48:28 -0500

    Subir Challenge-React-02

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import FirstApp from './FirstApp';
+import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode> <FirstApp/> </React.StrictMode>
+   <React.StrictMode> <FirstApp value = { 0 } /> </React.StrictMode>
 )
