8ac18add7b9ae14c0a40e0fdaa8e1fbaeb4bed82
Clavijo_110 <alejandro.clavijo_a@uao.edu.co>
2024-10-07 02:31:06 -0500

    Subir Challenge-React-05

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { GifExpertApp } from './GifExpertApp';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode> <GifExpertApp /> </React.StrictMode>
+   <React.StrictMode> <App /> </React.StrictMode>
 )
