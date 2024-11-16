a94b9b81a70595f9919b25eabd37c21574d12f4e
Clavijo_110 <alejandro.clavijo_a@uao.edu.co>
2024-11-16 12:45:34 -0500

    Subir Challenge-React-16

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import { db } from './firebase/config';
 import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import UserForm from './components/UserForm';
 import UserList from './components/UserList';
-import './App.css'
 
 function App() {
   const [users, setUsers] = useState([]);
 
   const usersCollectionRef = collection(db, "users");
 
-  // Create
   const createUser = async (name) => {
     await addDoc(usersCollectionRef, { name });
   };
 
-  // Read
   useEffect(() => {
     const unsubscribe = onSnapshot(usersCollectionRef, (snapshot) => {
       setUsers(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
@@ -23,13 +20,11 @@ function App() {
     return () => unsubscribe();
   }, []);
 
-  // Update
   const updateUser = async (id, newName) => {
     const userDoc = doc(db, "users", id);
     await updateDoc(userDoc, { name: newName });
   };
 
-  // Delete
   const deleteUser = async (id) => {
     const userDoc = doc(db, "users", id);
     await deleteDoc(userDoc);
